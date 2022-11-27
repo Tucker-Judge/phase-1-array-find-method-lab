010250e58afe2f538b450f660c5f12610b51a770
Tucker <tudge669@gmail.com>
2022-11-27 11:28:10 -0500

    commit tree 06e101080455c264815b98e000956ef979a73159

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// * The function should receive 1 argument, an `Array` of JavaScript `Object`s
+// * Each object has two properties: `year` and `result`
+// * It should use `find()` to test each `Object` to see if the `result` is `"W"` —
+//   a win!
+// * It should return the `year` when the win occurred (if it occurred at all!)
+// * If no win is found, it should return, sadly, `undefined`
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(arr) {
+    return arr.find(({result: "W"})) 
+  } 
+  
